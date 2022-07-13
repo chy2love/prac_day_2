2b437d3e781b446484c5dd890caa3ce8e8dfd323
Choi HY <chlgkduddlek@gmail.com>
2022-07-13 17:59:57 +0900

    day_3

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const user = [
+let user = [
   { name: '윤소희', age: 28, birthday: '0831', phoneNumber: '01025042452' },
   { name: '최하영', age: 25, birthday: '0209', phoneNumber: '01064089889' },
   { name: '이혜수', age: 32, birthday: '0406', phoneNumber: '01025042452' },
@@ -14,9 +14,20 @@ const user = [
 const userList = document.querySelector('#userList');
 const submitBtn = document.querySelector('#submitBtn');
 const checkedList = document.querySelector('#checkedList');
+const allCheckbox = document.querySelector('#allCheckbox');
 let checkedUsers = [];
 let paintedUser = [];
 let paintNewUser = [];
+let everyCheckbox;
+let count = 1;
+const date = new Date();
+for (let item of user) {
+  //user에 생일 추가
+  item.bornAt = `${String(date.getFullYear()) - item.age + 1}${item.birthday}`;
+  //user에 index 추가
+  item.index = count;
+  count++;
+}
 
 function paintDocument(user) {
   for (let item of user) {
@@ -33,24 +44,53 @@ function paintDocument(user) {
     userListEl.appendChild(userLabel);
     userCheckbox.addEventListener('change', handleCheckbox);
   }
+  everyCheckbox = document.querySelectorAll('.indivCheckbox');
+  allCheckbox.addEventListener('change', handleAllCheckbox);
+}
+function handleAllCheckbox() {
+  if (allCheckbox.checked === true) {
+    for (let item of everyCheckbox) {
+      if (item.disabled !== true) {
+        item.checked = true;
+        paintNewUser.push(
+          ...user.filter((element) => element.name === item.id)
+        );
+      }
+    }
+  } else {
+    for (let item of everyCheckbox) {
+      if (item.disabled !== true) {
+        item.checked = false;
+        paintNewUser = [];
+      }
+    }
+  }
 }
 function handleCheckbox(event) {
   if (event.target.checked) {
     for (let item of user) {
-      if (item.name === event.target.id && !checkedUsers.includes(item)) {
-        checkedUsers.push(item);
+      if (item.name === event.target.id && !paintNewUser.includes(item)) {
+        paintNewUser.push(item);
+        if (paintNewUser.length === user.length) {
+          allCheckbox.checked = true;
+        }
       }
     }
   } else {
-    checkedUsers = checkedUsers.filter((user) => user.name !== event.target.id);
+    allCheckbox.checked = false;
+    paintNewUser = paintNewUser.filter((user) => user.name !== event.target.id);
   }
 }
 
 function paintCheckedUser(paintList) {
+  paintList = paintList.sort(function (a, b) {
+    return a.index - b.index;
+  });
   for (let user of paintList) {
     const checkedListEl = document.createElement('li');
     const checkedListBtn = document.createElement('button');
     const checkedListSpan = document.createElement('span');
+    const disableCheckbox = document.getElementById(user.name);
     checkedListEl.className = user.name;
     checkedListBtn.type = 'button';
     checkedListBtn.innerText = 'X';
@@ -60,37 +100,27 @@ function paintCheckedUser(paintList) {
     checkedListEl.appendChild(checkedListBtn);
     checkedListEl.appendChild(checkedListSpan);
     paintedUser.push(user);
+    disableCheckbox.setAttribute('disabled', true);
     checkedListBtn.addEventListener('click', deleteUser);
   }
 }
 
 function handleSubmitBtn() {
-  let duplicatedUser = [];
-  for (let item of checkedUsers) {
-    if (paintedUser.includes(item)) {
-      duplicatedUser.push(item.name);
-    }
+  paintCheckedUser(paintNewUser);
+  for (let checkbox of everyCheckbox) {
+    checkbox.checked = false;
   }
-  if (duplicatedUser.length !== 0) {
-    alert('이미 선택된 유저입니다: ' + duplicatedUser);
-  } else {
-    for (let item of checkedUsers) {
-      paintNewUser.push(item);
-    }
-    paintCheckedUser(paintNewUser);
-    const everyCheckbox = document.querySelectorAll('.indivCheckbox');
-    for (let checkbox of everyCheckbox) {
-      checkbox.checked = false;
-    }
-    paintNewUser = [];
-    checkedUsers = [];
-  }
+  allCheckbox.checked = false;
+  paintNewUser = [];
+  console.log(checkedUsers);
   console.log(paintedUser);
 }
 
 function deleteUser(event) {
   for (let item of paintedUser) {
     if (item.name === event.target.parentElement.className) {
+      const enableCheckbox = document.getElementById(item.name);
+      enableCheckbox.disabled = false;
       paintedUser = paintedUser.filter((user) => user.name !== item.name);
       event.target.parentElement.remove();
     }
